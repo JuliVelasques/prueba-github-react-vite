c9e2a9197e1e1352f5b214a49e007abf90692a5f
Julián Rodrigues Velasques <julirodriguesvelasques@gmail.com>
2023-09-24 19:43:40 -0300

    Add news files

---
+++ prueba-github-react-vite/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='div-inicial'>
+        <h1 className='titulo-inicial'>Hola Mundo</h1>
+        <p className='parrafo-principal'>Bienvenido a mi pagina web de Prueba de GitHub</p>
+      </div>
+    </>
+  )
+}
+
+export default App
